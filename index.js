15f4a987114e9aa6608820769f3746f9a1aaa934
LasseV12 <84160130+theSlade@users.noreply.github.com>
2021-07-10 19:07:04 +0200

    new update

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-module.exports.conmasterDB = require("../src/functions/mongoDB");
+// Data
 const color = require("./data/colors.json");
 const emoji = require("./data/emojis.json");
 
+// DB
+module.exports.conmasterDB = require("./src/db/mongoDB");
+module.exports.Schema = require("./src/db/Schema");
+
+// Functions
+const passGen = require("./src/functions/passGen");
+
+// Exports
 module.exports = { color };
 module.exports = { emoji };
+module.exports = { passGen };
---
+++ src/functions/passGen.js
@@ -0,0 +1,15 @@
+/**
+ * Generates a password
+ * @param {Number} Length The length of the password characters
+ * @returns {Promise<String>}
+ */
+module.exports = (Length) => {
+  if (!Length) throw new TypeError("Length isn't specified");
+  var length = Length,
+    res = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+    value = "";
+  for (var i = 0, n = res.length; i < length; ++i) {
+    value += res.charAt(Math.floor(Math.random() * n));
+  }
+  return value;
+};
