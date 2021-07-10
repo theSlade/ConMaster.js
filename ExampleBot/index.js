8b19531eaf9e80781f3de01280ee9ed3e78cde2c
LasseV12 <84160130+theSlade@users.noreply.github.com>
2021-07-10 20:14:53 +0200

    Update

--- ExampleBot/index.js
+++ ExampleBot/index.js
@@ -2,14 +2,27 @@ require("dotenv").config();
 const { Client, MessageEmbed } = require("discord.js");
 const { color, emoji } = require("conmaster.js");
 const bot = new Client();
+const words = ["Fuck", "Pussy", "Bitch", "fuck", "tissemand", "dick"]
+
+function ready() {
+  console.log("Ready!")
+}
+
+bot.on("ready", () => {
+  ready();
+  bot.user.setActivity("https://npmjs.com/conmaster.js", { type: "PLAYING" })
+})
 
 bot.on("message", (message) => {
   if (message.content === "!hello") {
     return message.reply(
       new MessageEmbed()
-        .setDescription("Hello there!" + emoji.happy1)
-        .setColor(color.white)
+        .setDescription(`${emoji.love2} Hello there!`)
+        .setColor(color.DarkBlue)
     );
+  } if (message.content.includes(`${words}`)) {
+     message.delete()
+     message.channel.send(`${emoji.laughing2} Slettede din bedsked!`)
   }
 });
 
