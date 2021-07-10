4a32235750673b413dcc617b27e4d3e42bf25f58
LasseV12 <84160130+theSlade@users.noreply.github.com>
2021-07-10 16:10:18 +0200

    Push

--- ExampleBot/index.js
+++
@@ -1,8 +0,0 @@
-const { Client, MessageEmbed } = require("discord.js");
-const bot = new Client();
-
-bot.on("message", (message) => {
-  if (message.content === "!hello") {
-    return message.channel.send(new MessageEmbed().setColor());
-  }
-});
--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ module.exports.conmasterDB = require("../src/functions/mongoDB");
 const color = require("./data/colors.json");
 
 module.exports = { color };
+
+// Test push
